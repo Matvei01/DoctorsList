1dbe3c593c762303daa09b6580294dc0750d085a
Matvei <khlestov.matvei@mail.ru>
2024-08-20 15:28:38 +0500

    Added SF Pro Display fonts for app

---
+++ DoctorsList/DoctorsList/Extensions/Extension + Font.swift
@@ -0,0 +1,86 @@
+//
+//  Extension + Font.swift
+//  DoctorsList
+//
+//  Created by Matvei Khlestov on 20.08.2024.
+//
+
+import SwiftUI
+
+extension Font {
+    struct SFProDisplay {
+        static func black(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-Black", size: size)
+        }
+        
+        static func blackItalic(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-BlackItalic", size: size)
+        }
+        
+        static func bold(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-Bold", size: size)
+        }
+        
+        static func boldItalic(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-BoldItalic", size: size)
+        }
+        
+        static func heavy(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-Heavy", size: size)
+        }
+        
+        static func heavyItalic(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-HeavyItalic", size: size)
+        }
+        
+        static func light(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-Light", size: size)
+        }
+        
+        static func lightItalic(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-LightItalic", size: size)
+        }
+        
+        static func medium(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-Medium", size: size)
+        }
+        
+        static func mediumItalic(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-MediumItalic", size: size)
+        }
+        
+        static func regular(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-Regular", size: size)
+        }
+        
+        static func regularItalic(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-RegularItalic", size: size)
+        }
+        
+        static func semibold(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-Semibold", size: size)
+        }
+        
+        static func semiboldItalic(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-SemiboldItalic", size: size)
+        }
+        
+        static func thin(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-Thin", size: size)
+        }
+        
+        static func thinItalic(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-ThinItalic", size: size)
+        }
+        
+        static func ultralight(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-Ultralight", size: size)
+        }
+        
+        static func ultralightItalic(size: CGFloat) -> Font {
+            return Font.custom("SF-Pro-Display-UltralightItalic", size: size)
+        }
+    }
+}
+
+
