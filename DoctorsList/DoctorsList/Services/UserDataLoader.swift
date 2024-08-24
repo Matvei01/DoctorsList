cad73a5fe3bac763ec9090795752eaec4ce8feda
Matvei <khlestov.matvei@mail.ru>
2024-08-24 05:51:29 +0500

    Added UserDataLoader for loading data from JSON file

---
+++ DoctorsList/DoctorsList/Services/UserDataLoader.swift
@@ -0,0 +1,69 @@
+//
+//  UserDataLoader.swift
+//  DoctorsList
+//
+//  Created by Matvei Khlestov on 23.08.2024.
+//
+
+import Foundation
+
+final class UserDataLoader {
+    static let shared = UserDataLoader()
+    
+    private init() {}
+    
+    func loadUsers(completion: @escaping (Result<[User], JSONLoadError>) -> Void) {
+        guard let url = Bundle.main.url(forResource: Link.allUsers.fileName, withExtension: "json") else {
+            completion(.failure(.fileNotFound))
+            return
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            let rootResponse = try decoder.decode(RootResponse.self, from: data)
+            let users = rootResponse.record.data.users
+            
+            DispatchQueue.main.async {
+                completion(.success(users))
+            }
+            
+        } catch let error as DecodingError {
+            DispatchQueue.main.async {
+                completion(.failure(.decodingFailed(error)))
+            }
+            
+        } catch {
+            DispatchQueue.main.async {
+                completion(.failure(.dataLoadingFailed(error)))
+            }
+        }
+    }
+}
+
+// MARK: - JSONLoadError
+extension UserDataLoader {
+    enum JSONLoadError: Error {
+        case fileNotFound
+        case dataLoadingFailed(Error)
+        case decodingFailed(Error)
+    }
+}
+
+// MARK: - Link
+extension UserDataLoader {
+    enum Link {
+        case allUsers
+        
+        var fileName: String {
+            switch self {
+            case .allUsers:
+                return "655b754e0574da7622c94aa4"
+            }
+        }
+    }
+}
+
